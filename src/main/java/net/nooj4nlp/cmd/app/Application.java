1f4d1e754d141de797466f8276dfb12f73ab68c7
tkb <@>
2014-03-22 21:45:21 +0100

    Added main class and refactored TextProcessor

---
+++ src/main/java/net/nooj4nlp/cmd/app/Application.java
@@ -0,0 +1,35 @@
+package net.nooj4nlp.cmd.app;
+
+import java.io.IOException;
+
+import net.nooj4nlp.cmd.io.CharVariantsLoader.CharVariantsException;
+import net.nooj4nlp.cmd.io.LinguisticResources.LinguisticResourceException;
+import net.nooj4nlp.cmd.io.ResourceLinker.LinkerException;
+import net.nooj4nlp.cmd.io.TextLoader.TextLoaderException;
+import net.nooj4nlp.cmd.processing.LexicalAnalyzer.LexicalAnalysisException;
+import net.nooj4nlp.cmd.processing.SyntacticParser.GrammarException;
+import net.nooj4nlp.cmd.processing.SyntacticParser.SyntaxParsingException;
+import net.nooj4nlp.cmd.processing.TextDelimiter.DelimiterException;
+
+import org.apache.commons.cli.ParseException;
+
+class Application {
+	public static void main(String[] args) throws IOException, ParseException {
+		StaticInitialization.initialize();
+		
+		try {
+			NoojOptions noojOptions = NoojOptions.create(args);
+			TextProcessor textProcessor = TextProcessor.create(noojOptions);
+			textProcessor.processFiles(noojOptions.getFiles());
+		} catch (ParseException e) {
+		} catch (CharVariantsException e) {
+		} catch (LinguisticResourceException e) {
+		} catch (LinkerException e) {
+		} catch (TextLoaderException e) {
+		} catch (DelimiterException e) {
+		} catch (LexicalAnalysisException e) {
+		} catch (SyntaxParsingException e) {
+		} catch (GrammarException e) {
+		}
+	}
+}
--- src/main/java/net/nooj4nlp/cmd/app/TextProcessor.java
+++ src/main/java/net/nooj4nlp/cmd/app/TextProcessor.java
@@ -1,7 +1,7 @@
 package net.nooj4nlp.cmd.app;
 
-import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import net.nooj4nlp.cmd.io.CharVariantsLoader;
@@ -10,10 +10,12 @@ import net.nooj4nlp.cmd.io.LinguisticResources;
 import net.nooj4nlp.cmd.io.TextLoader;
 import net.nooj4nlp.cmd.processing.LexicalAnalyzer;
 import net.nooj4nlp.cmd.processing.Ntext2Xml;
+import net.nooj4nlp.cmd.processing.NtextImporter;
 import net.nooj4nlp.cmd.processing.NtextProcessor;
 import net.nooj4nlp.cmd.processing.RawText2Ntext;
 import net.nooj4nlp.cmd.processing.SyntacticParser;
 import net.nooj4nlp.cmd.processing.TextDelimiter;
+import net.nooj4nlp.cmd.processing.Xml2Ntext;
 import net.nooj4nlp.engine.Engine;
 import net.nooj4nlp.engine.Language;
 import net.nooj4nlp.engine.Ntext;
@@ -22,46 +24,66 @@ import net.nooj4nlp.engine.RefObject;
 import com.google.common.collect.ImmutableList;
 
 public class TextProcessor {
-	private Language language;
-	private List<File> files;
-	private List<Path> lexicalResources;
-	private List<Path> syntacticResources;
-	private List<String> xmlAnnotations;
-	private Path propertiesDefinitions;
-	private File charVariantsFile;
-	private String delimiter;
-	private Encoding encoding;
-	private boolean filterXml;
-	private Path outputDirectory;
-	private List<String> xmlTags;
+	private static final String XML_EXTENSION = ".xml";
+	
+	private final TextLoader textIO;
+	private final NtextImporter converter;
+	private final LinguisticResources resources;
+	private final List<NtextProcessor> ntextProcessors;
+	private final Ntext2Xml xmlConverter;
 
-	public void process() {
-		new CharVariantsLoader(charVariantsFile).loadInto(language);
+	public TextProcessor(TextLoader textIO,
+			NtextImporter inputConverter,
+			LinguisticResources resources,
+			List<NtextProcessor> ntextProcessors,
+			Ntext2Xml xmlConverter) {
+		this.textIO = textIO;
+		this.converter = inputConverter;
+		this.resources = resources;
+		this.ntextProcessors = ntextProcessors;
+		this.xmlConverter = xmlConverter;
+	}
+	
+	public static TextProcessor create(NoojOptions options) {
+		Language language = options.getLanguage();
+		Encoding encoding = options.getEncoding();
+		
+		Path characterVariants = options.getCharVariantsFile();
+		if (characterVariants != null) {
+			new CharVariantsLoader(characterVariants).loadInto(language);
+		}
+		
 		TextLoader textIO = new TextLoader(encoding, language);
+		
+		List<String> xmlTags = options.getXmlTags();
+		NtextImporter inputConverter;
+		if (xmlTags == null) {
+			inputConverter = new RawText2Ntext(language, options.getDelimiter());
+		} else {
+			inputConverter = new Xml2Ntext(language, xmlTags);
+		}
+		
+		List<Path> lexicalResources = options.getLexicalResources();
+		List<Path> syntacticResources = options.getSyntacticResources();
+		Path propertiesDefinitions = options.getPropertiesDefinitions();
+		Path tmpDirectory = options.getTmpDirectory();
+		
 		LinguisticResources resources =
 				new LinguisticResources(lexicalResources,
 						syntacticResources,
 						propertiesDefinitions,
-						outputDirectory);
+						tmpDirectory);
+		
 		List<NtextProcessor> ntextProcessors = createNtextProcessors(language, resources);
-		RawText2Ntext rawTextConverter = new RawText2Ntext(language, delimiter);
+
+		List<String> xmlAnnotations = options.getXmlAnnotations();
+		boolean filterXml = options.isFilterXml();
 		Ntext2Xml xmlConverter = new Ntext2Xml(xmlAnnotations, language, filterXml);
 		
-		for (File file : files) {
-			String rawText = textIO.load(file);
-			Ntext nText = rawTextConverter.convert(rawText);
-			resources.loadInto(nText);
-			
-			for (NtextProcessor processor : ntextProcessors) {
-				processor.process(nText);
-			}
-			
-			String xml = xmlConverter.convert(nText);
-			textIO.write(xml, null);
-		}
+		return new TextProcessor(textIO, inputConverter, resources, ntextProcessors, xmlConverter);
 	}
-
-	private List<NtextProcessor> createNtextProcessors(Language language, LinguisticResources resources) {
+	
+	private static List<NtextProcessor> createNtextProcessors(Language language, LinguisticResources resources) {
 		Engine engine = new Engine(new RefObject<Language>(language),
 				"", "", "",
 				false, null, false, null);
@@ -71,5 +93,21 @@ public class TextProcessor {
 		return ImmutableList.of(new TextDelimiter(engine),
 				new LexicalAnalyzer(engine),
 				new SyntacticParser(engine));
+	}
+
+	void processFiles(List<Path> files) {
+		for (Path file : files) {
+			String rawText = textIO.load(file);
+			Ntext nText = converter.convert(rawText);
+			resources.loadInto(nText);
+			
+			for (NtextProcessor processor : ntextProcessors) {
+				processor.process(nText);
+			}
+			
+			String xml = xmlConverter.convert(nText);
+			String outputFileName = file.toAbsolutePath().toString() + XML_EXTENSION;
+			textIO.write(xml, Paths.get(outputFileName));
+		}
 	}
 }
